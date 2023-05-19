94fe7638a7093c3e07e177aa95dcff7acaf868a0
Ang Kok Leong <angkokleong@Apex.local>
2023-05-19 20:22:05 +0800

    initial commit for project setup

---
+++ src/main/java/com/libraryportalrest/LibraryPortalRestApplication.java
@@ -0,0 +1,13 @@
+package com.libraryportalrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPortalRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPortalRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libraryportalrest/LibraryPortalRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryportalrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryPortalRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
