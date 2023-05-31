8f0e9598811e0203c2e4856123df4d1230786e12
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-05-31 00:25:42 +0000

    LIB-164 added the implementation of the backend REST endpoint for Book Search

---
+++ src/main/java/com/libraryportal/restapi/config/RestApiConfig.java
@@ -0,0 +1,13 @@
+package com.libraryportal.restapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+//import com.libraryportal.restapi.dao.ExtendedRepositoryImpl;
+
+/* @Configuration
+@EnableJpaRepositories(basePackages = "org.baeldung.persistence.dao", repositoryBaseClass = ExtendedRepositoryImpl.class)
+public class RestApiConfig {
+    
+}
+*/
--- src/main/java/com/libraryportal/restapi/dao/BookRepository.java
+++ src/main/java/com/libraryportal/restapi/dao/BookRepository.java
@@ -1,9 +1,16 @@
 package com.libraryportal.restapi.dao;
 
+import java.io.Serializable;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.libraryportal.restapi.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer>{
+    
+    Page<Book> findBookByTitleContaining(@RequestParam("title") String title, Pageable pageable);
     
 }
