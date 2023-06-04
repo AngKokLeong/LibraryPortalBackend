b19fb3470d26ba59dc489f7f7aa94fc037f8efbc
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-04 11:07:10 +0000

    LIB-175 modified the Review REST API endpoint by including the findBookByBookId search endpoint in Spring Boot

--- src/main/java/com/libraryportal/restapi/dao/ReviewRepository.java
+++ src/main/java/com/libraryportal/restapi/dao/ReviewRepository.java
@@ -1,8 +1,14 @@
 package com.libraryportal.restapi.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.libraryportal.restapi.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+    Page<Review> findBookByBookId(@RequestParam("bookId") Integer bookId, Pageable pageable);
     
 }
