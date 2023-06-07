6d6d94d281d96e0b353bc0cc16abde4a95b80928
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-07 12:41:34 +0000

    LIB-193 added Checkout repository

---
+++ src/main/java/com/libraryportal/restapi/dao/CheckoutRepository.java
@@ -0,0 +1,8 @@
+package com.libraryportal.restapi.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.libraryportal.restapi.entity.Checkout;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Integer>{
+    Checkout findBookCheckoutByUserEmailAndBookId(String userEmail, Long bookId);
+}
