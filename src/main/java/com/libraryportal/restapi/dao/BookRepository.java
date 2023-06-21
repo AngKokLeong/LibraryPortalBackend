af67ae423ca3baaa1b7a41f4286fa90efc161e57
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-21 11:35:30 +0000

    LIB-229 added findBooksByBookIds method using Query

--- src/main/java/com/libraryportal/restapi/dao/BookRepository.java
+++ src/main/java/com/libraryportal/restapi/dao/BookRepository.java
@@ -1,8 +1,11 @@
 package com.libraryportal.restapi.dao;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.libraryportal.restapi.entity.Book;
@@ -12,4 +15,7 @@ public interface BookRepository extends JpaRepository<Book, Integer>{
     Page<Book> findBookByTitleContaining(@RequestParam("title") String title, Pageable pageable);
     
     Page<Book> findBookByCategory(@RequestParam("category") String category, Pageable pageable);
+
+    
+    List<Book> findBooksByBookIds(@Param("book_ids") List<Integer> bookId);
 }
