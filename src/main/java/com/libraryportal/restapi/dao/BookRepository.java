723603c59702f63e2ef186a52f31ccc3bbe26143
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-21 12:19:54 +0000

    LIB-229 added the @Query annotation and its import

--- src/main/java/com/libraryportal/restapi/dao/BookRepository.java
+++ src/main/java/com/libraryportal/restapi/dao/BookRepository.java
@@ -1,10 +1,12 @@
 package com.libraryportal.restapi.dao;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +18,7 @@ public interface BookRepository extends JpaRepository<Book, Integer>{
     
     Page<Book> findBookByCategory(@RequestParam("category") String category, Pageable pageable);
 
-    
+    @Query("select o from Book o where id in :book_ids")
     List<Book> findBooksByBookIds(@Param("book_ids") List<Integer> bookId);
+
 }
