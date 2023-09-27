f19252be21b44c5d7c30db42b1c81cdb8541e6b8
Ang Kok Leong <53397893+AngKokLeong@users.noreply.github.com>
2023-09-27 09:18:37 +0000

    LIB-267 added postgresql dependency into pom.xml

--- sql_script.java
+++ sql_script.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-
+import java.util.ArrayList;
 
 public class PostgreSQLJDBC {
    public static void main( String args[] ) {
