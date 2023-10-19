9a09ef064849079412afe7bea9e85881b71a6e01
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2023-10-19 08:26:51 +0000

    LIB-316 modified Checkout and Review Repository to remove all records relating to one book_id

--- src/main/java/com/libraryportal/restapi/dao/CheckoutRepository.java
+++ src/main/java/com/libraryportal/restapi/dao/CheckoutRepository.java
@@ -3,9 +3,17 @@ package com.libraryportal.restapi.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.libraryportal.restapi.entity.Checkout;
 
 public interface CheckoutRepository extends JpaRepository<Checkout, Integer>{
     Checkout findBookCheckoutByUserEmailAndBookId(String userEmail, Integer bookId);
     List<Checkout> findBookCheckoutByUserEmail(String userEmail);
+
+    @Modifying
+    @Query("delete from Checkout where id in :book_id")
+    void deleteAllByBookId(@Param("book_id") Integer bookId);
 }
--- src/main/java/com/libraryportal/restapi/dao/ReviewRepository.java
+++ src/main/java/com/libraryportal/restapi/dao/ReviewRepository.java
@@ -3,6 +3,9 @@ package com.libraryportal.restapi.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.libraryportal.restapi.entity.Review;
@@ -13,4 +16,7 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
     
     Review findBookReviewByUserEmailAndBookId(String userEmail, Integer bookId);
 
+    @Modifying
+    @Query("delete from Review where id in :book_id")
+    void deleteAllByBookId(@Param("book_id") Integer book_id);
 }
