e7be9fb6f9ba95e1cd93471118663141c1a536a5
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2023-10-25 15:03:30 +0000

    LIB-345 added /payment-intent endpoint

--- src/main/java/com/libraryportal/restapi/controller/PaymentController.java
+++ src/main/java/com/libraryportal/restapi/controller/PaymentController.java
@@ -1,11 +1,18 @@
 package com.libraryportal.restapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.libraryportal.restapi.requestmodels.PaymentInformationRequest;
 import com.libraryportal.restapi.service.PaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @CrossOrigin("https://localhost:3000")
 @RestController
@@ -17,5 +24,15 @@ public class PaymentController {
     @Autowired
     public PaymentController(PaymentService paymentService){
         this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInformationRequest paymentInformationRequest) throws StripeException{
+        
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInformationRequest);
+        
+        String paymentIntentJsonString = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentIntentJsonString, HttpStatus.OK);
     }
 }
